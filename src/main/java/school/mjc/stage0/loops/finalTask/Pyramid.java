5d472d9a9f1836027ac545f7c7b3baa47a6789e8
Nikita Nasevich <nasevich.nik@mail.ru>
2022-06-29 10:53:46 +0300

    solved

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,10 +2,29 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printHalfPyramid(int cathetusLength) {
+        int minus;
+        for (int i = 0; i < cathetusLength; i++) {
+            minus = i + 1;
+            for (int j = 0; j < cathetusLength * 2; j++) {
+                if (j >= cathetusLength - i && j <= cathetusLength + i) {
+                    if (j <= cathetusLength) {
+                        System.out.print(minus);
+                        minus--;
+                    } else {
+                        minus++;
 
+                        System.out.print(minus + 1);
+
+                    }
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
-        new Pyramid().printHalfPyramid(7);
+        new Pyramid().printHalfPyramid(9);
     }
 }
